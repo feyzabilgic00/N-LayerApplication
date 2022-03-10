99580da3280f67d954a16c8d587cfd4a6726a365
feyzabilgic00@gmail.com <feyzabilgic00@gmail.com>
2022-03-11 00:22:01 +0300

    Add project files.

---
+++ N-Layer.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace N_Layer.Core.Entities
+{
+    /* BaseEntity classından nesne örneği üretilmesini engellemek için abstract olarak tanımladık.*/
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
